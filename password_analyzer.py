927fdda5bdaede7fb8cf29f98347bffbdb95aecd
Muhammed Emin Şeker <muhammedeminsekerr@gmail.com>
2026-08-29 20:26:40 +0300

    Add password_analyzer.py skeleton with TODO stubs

---
+++ password_analyzer.py
@@ -0,0 +1,17 @@
+import re
+
+def check_length(password):
+    # TODO: uzunluk puani (8+ orta, 12+ iyi, 16+ cok iyi)
+    pass
+
+def check_variety(password):
+    # TODO: kucuk harf, buyuk harf, rakam, ozel karakter var mi
+    pass
+
+def analyze(password):
+    # TODO: tum kontrolleri birlestir, puan ve seviye dondur
+    pass
+
+if __name__ == "__main__":
+    # TODO: ornek sifreleri test et
+    pass
